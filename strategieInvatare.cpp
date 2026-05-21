f73fd1fb3bb6eb915ebe4b39ec1ff09631c7803d
DorinMeteleanu <dorinmeteleanu06@gmail.com>
2026-05-21 15:32:49 +0300

    logica Strategy pattern pentru invatare

---
+++ strategieInvatare.cpp
@@ -0,0 +1,35 @@
+#include "strategieInvatare.h"
+#include "ui_util.h"
+
+void StrategieHardcore::aplicaStrategie(int ore, int& energie, int& stres, int& bonus) {
+    energie -= ore*3;
+    stres += ore*5;
+    bonus += ore*4;
+    std::cout << MAGENTA << "[STRATEGIE] Bagi tare! Ochi rosii, dar retii tot!\n" << RESET;
+};
+
+int StrategieHardcore::getCooldownValue() const{
+    return 3;
+};
+
+void StrategieNormala::aplicaStrategie(int ore, int& energie, int& stres, int& bonus) {
+    energie -= ore*2;
+    stres += ore*3;
+    bonus += ore*2;
+    std::cout << MAGENTA << "[STRATEGIE] Ai invatat normal. Echilibrat.\n" << RESET;
+};
+
+int StrategieNormala::getCooldownValue() const{
+    return 2;
+};
+
+void StrategieRelaxata::aplicaStrategie(int ore, int& energie, int& stres, int& bonus) {
+    energie -= ore*1;
+    stres += ore*1;
+    bonus += ore/2;
+    std::cout << "[STRATEGIE] Ai invatat cu muzica pe fundal. Ai gusturi ciudate. Mai mult te-ai relaxat.\n";
+};
+
+int StrategieRelaxata::getCooldownValue() const{
+    return 1;
+};
---
+++ strategieInvatare.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+
+class StrategieInvatare {
+public:
+    virtual void aplicaStrategie(int ore, int& energie, int& stres, int& bonus) = 0;
+    virtual int getCooldownValue() const = 0;
+    virtual ~StrategieInvatare();
+};
+
+class StrategieHardcore : public StrategieInvatare {
+public:
+    void aplicaStrategie(int ore, int& energie, int& stres, int& bonus) override;
+    int getCooldownValue() const;
+};
+
+class StrategieRelaxata : public StrategieInvatare {
+    void aplicaStrategie(int ore, int& energie, int& stres, int& bonus) override;
+    int getCooldownValue() const;
+};
+
+class StrategieNormala : public StrategieInvatare {
+    void aplicaStrategie(int ore, int& energie, int& stres, int& bonus) override;
+    int getCooldownValue() const;
+};
